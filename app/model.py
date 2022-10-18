3215c6b576779cc4173683ddaf908f0e993910c7
nicoromero <nicoromero07@hotmail.com>
2022-10-18 12:28:22 +0200

    update models

--- app/model.py
+++ app/model.py
@@ -1,3 +1,4 @@
+from datetime import date
 from pydantic import BaseModel
 from typing import Union, List, Optional
 
@@ -5,14 +6,48 @@ class Address(BaseModel):
     street: str
     number: str
     floor: Optional[str]
-    coordinates: List[float]
+    geojson: List[float]
+    postal_code: str
     details: str
 
+class User(BaseModel):
+    username: str
+    first_name: str
+    last_name: str
+    email: str
+
+class Period(BaseModel):
+    start: date
+    end: date
+
 class Household(BaseModel):
-    name: str
-    description: Optional[str] = None
-    num_rooms: int
-    num_bathrooms: int
-    num_beds: int
-    photo: Optional[str] = None
+    '''HOST'''
+    host_username : str
+    host_email: str
+    title: str
+    description: str
     address: Address
+    photo: List[str]
+    num_bathroom: int
+    num_bed: int
+    max_capacity: int
+    price_euro_per_night: float
+    rating: float
+    availability: List[Period]
+
+
+class Booking(BaseModel):
+    period: Period
+
+    '''HOST'''
+    host_email: str
+
+    '''RENTER'''
+    renter_username: str
+    renter_email: str
+
+    '''HOUSEHOLD'''
+    household_tittle: str
+    household_address_street: str
+    household_address_number: int
+    household_address_postal_code: str
