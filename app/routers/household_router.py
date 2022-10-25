e419a59b7808afa25504f860a299a6e29e4c1e03
nicor <nicoromero07@uma.es>
2022-10-25 10:30:40 +0200

    Update household_router

--- app/routers/household_router.py
+++ app/routers/household_router.py
@@ -1,23 +1,15 @@
 from fastapi import APIRouter, Body, Request, Response, HTTPException, status
 from fastapi.encoders import jsonable_encoder
-from pymongo import MongoClient
-from dotenv import dotenv_values
 from typing import List
-
-import uvicorn
 
 from model import Household
 
 router = APIRouter()
 
-@router.get("/hello")
-def hello_world(request: Request):
-    return { "hello_world ": "hello_world"}
-
-
 '''CREATE HOUSEHOLD'''
 @router.post("/", response_description="Create a new household", status_code=status.HTTP_201_CREATED, response_model=Household)
 def create_household(request: Request, household: Household = Body(...)):
+
     household = jsonable_encoder(household)
     new_household = request.app.database["household"].insert_one(household)
     created_household = request.app.database["household"].find_one(
@@ -34,7 +26,7 @@ def list_households(request: Request):
     
 '''GET HOUSEHOLD'''
 @router.get("/{id}", response_description="Get a single household", response_model=Household)
-def get_household(request: Request):
+def get_household(id:str, request: Request):
     if(household := request.app.database["household"].find_one({"_id": id})) is not None:
         return household
 
@@ -42,7 +34,7 @@ def get_household(request: Request):
 
 '''DELETE HOUSEHOLD'''
 @router.delete("/{id}", response_description="Delete a household")
-def delete_household(request: Request, response: Response):
+def delete_household(id:str, request: Request, response: Response):
     household_deleted = request.app.database["household"].delete_one({"_id": id})
 
     if household_deleted.deleted_count:
