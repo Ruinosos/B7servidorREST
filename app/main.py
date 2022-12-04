aa2eae61e4841049cdcfca8699c0fb4b795112dc
VictorMadrona <95759313+VictorMadrona@users.noreply.github.com>
2022-12-04 16:01:48 +0100

    CRUD comments

--- app/main.py
+++ app/main.py
@@ -7,6 +7,7 @@ from routers.household_router import router as household_router
 from routers.book_router import router as book_router
 from routers.address_router import router as address_router
 from routers.users_router import router as user_router
+from routers.comment_router import router as comment_router
 from Imgur.Imgur import authenticate
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -42,6 +43,7 @@ app.include_router(household_router, tags=["households"], prefix="/households")
 app.include_router(book_router, tags=["bookings"], prefix="/bookings")
 app.include_router(address_router, tags=["addresses"], prefix="/addresses")
 app.include_router(user_router, tags=["users"], prefix="/users")
+app.include_router(comment_router, tags=["comments"], prefix="/comments")
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8001, reload=True)
